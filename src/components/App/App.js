dce0675363100b202f920bbabe3da64b01614a5a
Leonel Mandarino <leonelmandarino@gmail.com>
2020-02-22 23:38:49 -0300

    Add general routing and private route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,71 @@
-import React from 'react';
-import Login from '../Login'
+import React, { useState } from 'react';
+import Login from '../Login';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+} from 'react-router-dom';
+import PrivateRoute from '../../routes'
+import { AuthContext, useAuth } from '../../helpers/context'
 
-function App(props) {
+function App() {
+    // Try to set the tokens if there are
+    const tokens = localStorage.getItem('tokens');
+    const [authTokens, setAuthTokens] = useState(tokens);
+
+    function setTokens(data, remember) {
+        if (data) {
+            if (remember) {
+                localStorage.setItem('tokens', JSON.stringify(data));
+            }
+        } else {
+            localStorage.removeItem('tokens');
+        }
+        setAuthTokens(data);
+    }
+
     return (
-        <Login />
+        <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
+            <Router>
+                <Switch>
+                    <Route path='/login' component={Login} />
+                    <Route path='/register' component={Register} />
+                    <PrivateRoute path='/products' component={Products} />
+                    <Route component={NotFound} />
+                </Switch>
+            </Router>
+        </AuthContext.Provider>
     );
+}
+
+function Register(props) {
+    return <h2>Termino esto y me hago un cafecito con tostada</h2>
+}
+
+function Products(props) {
+    const { setAuthTokens } = useAuth();
+
+    function handleLogout() {
+        setAuthTokens();
+    }
+    return (
+        <div>
+            <h2>hola que hace</h2>
+            <button onClick={handleLogout}>Logout</button>
+        </div>
+    )
+}
+
+function NotFound(props) {
+    return (
+        <div>
+            <h1>404 Page Not Found</h1>
+            <Link to='/'>
+                <button>Ir a casa</button>
+            </Link>
+        </div>
+    )
 }
 
 export default App;
