8764cbbc9c894d529eea10a5865f9d578317cc63
Leonel <leonelmandarino@gmail.com>
2021-06-11 20:46:35 -0300

    Add measure endpoint

--- src/services/currentUser.js
+++ src/services/currentUser.js
@@ -1,30 +1,32 @@
-import axios from './index';
-import {fetchMultipleLinks} from './utils'
+import axios from "./index";
+import { fetchMultipleLinks } from "./utils";
 
 export function getUser() {
-    return axios.get('auth/users/me/').then(response => response.data);
+  return axios.get("auth/users/me/").then((response) => response.data);
 }
 
 export async function getProducts() {
-    let product_set = await getUser().then(user => user.product_set);
-    return fetchMultipleLinks(product_set);
+  let product_set = await getUser().then((user) => user.product_set);
+  return fetchMultipleLinks(product_set);
 }
 
 export async function getSubproducts() {
-    let subproduct_set = await getUser().then(user => user.subproduct_set);
-    return fetchMultipleLinks(subproduct_set)
+  let subproduct_set = await getUser().then((user) => user.subproduct_set);
+  return fetchMultipleLinks(subproduct_set);
 }
 
 export async function getPackagingObjects() {
-    let packagingobject_set = await getUser().then(user => user.packagingobject_set);
-    return fetchMultipleLinks(packagingobject_set)
+  let packagingobject_set = await getUser().then(
+    (user) => user.packagingobject_set
+  );
+  return fetchMultipleLinks(packagingobject_set);
 }
 
 export async function loginUser(body) {
-    const {data} = await axios.post(`auth/jwt/create/`, body);
-    return data;
+  const { data } = await axios.post(`auth/jwt/create/`, body);
+  return data;
 }
 
 export async function registerUser(body) {
-    return await axios.post(`auth/users/`, body);
+  return await axios.post(`auth/users/`, body);
 }
--- src/services/products.js
+++ src/services/products.js
@@ -1,52 +1,60 @@
-import axios from './index'
-import {fetchMultipleLinks} from './utils'
+import axios from "./index";
+import { fetchMultipleLinks } from "./utils";
 
 export function getProductWithId(id) {
-    return axios.get(`/api/products/${id}`);
+  return axios.get(`/api/products/${id}`);
 }
 
 export function getSubProductWithId(id) {
-    return axios.get(`/api/subproducts/${id}`);
+  return axios.get(`/api/subproducts/${id}`);
 }
 
 export function getPackagingObjectWithId(id) {
-    return axios.get(`/api/packaging/${id}`);
+  return axios.get(`/api/packaging/${id}`);
 }
 
 export function createProduct(body) {
-    return axios.post('/api/products/', body);
+  return axios.post("/api/products/", body);
 }
 
 export function createSubproduct(body) {
-    return axios.post('/api/subproducts/', body);
+  return axios.post("/api/subproducts/", body);
 }
 
 export function createPackagingObject(body) {
-    return axios.post('/api/packaging/', body);
+  return axios.post("/api/packaging/", body);
 }
 
 export function createComposition(body) {
-    return axios.post('/api/compositions/', body);
+  return axios.post("/api/compositions/", body);
 }
 
 export function deleteProduct(id) {
-    return axios.delete(`/api/products/${id}`)
+  return axios.delete(`/api/products/${id}`);
 }
 
 export function deleteSubproduct(id) {
-    return axios.delete(`/api/subproducts/${id}`)
+  return axios.delete(`/api/subproducts/${id}`);
 }
 
 export function deletePackagingObject(id) {
-    return axios.delete(`/api/packaging/${id}`)
+  return axios.delete(`/api/packaging/${id}`);
 }
 
 export function deleteComposition(id) {
-    return axios.delete(`/api/compositions/${id}`)
+  return axios.delete(`/api/compositions/${id}`);
 }
 
 export function getCompositionsOfProduct(product) {
-    const {compositions} = product;
-    return fetchMultipleLinks(compositions);
+  const { compositions } = product;
+  return fetchMultipleLinks(compositions);
 }
 
+export function createMeasure(body) {
+  return axios.post("/api/measures/", body);
+}
+
+export function getMeasuresOfProduct(product) {
+  const { measures } = product;
+  return fetchMultipleLinks(measures);
+}
